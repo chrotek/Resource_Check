20b7f2dc65b11d3438f5032db96e0ec8b571f078
chrotek <chrotek@gmail.com>
2019-05-31 15:26:16 +0200

    memory awks, fixed

--- resourcecheck.sh
+++ resourcecheck.sh
@@ -18,9 +18,10 @@ totalMemory=$(grep "MemTotal" /proc/meminfo | awk {'print $2'})
 freeMemory=$(grep "MemFree" /proc/meminfo | awk {'print $2'})
 availableMemory=$(grep "MemAvailable" /proc/meminfo | awk {'print $2'})
 usedMemory=$((totalMemory - availableMemory))
-# usedMemoryPercent=$(((usedMemory / totalMemory)*100))
-usedMemoryPercent_bc=$(printf '%.3f\n' $(echo "$usedMemory / $totalMemory" | bc -l ))
-usedMemoryPercent=$(printf "\n" |awk "{printf ($usedMemory / $totalMemory)*100 ;exit}")
+#usedMemoryPercent_bc=$(printf '%.3f\n' $(echo "$usedMemory / $totalMemory" | bc -l ))
+#usedMemoryPercent=$(printf "\n" |awk "{printf ($usedMemory / $totalMemory)*100}")
+usedMemoryPercent=$(printf "\n" |awk "{printf ($usedMemory / $totalMemory)*100 ;exit}"| awk '{printf "%.2f\n", $1}')
+
 ### CPU
 
 ### DISK Space
@@ -32,19 +33,6 @@ printf "Total Memory    : %s \n" "$totalMemory"
 printf "Free Memory     : %s \n" "$freeMemory"
 printf "Available Memory: %s \n" "$availableMemory"
 printf "Used Memory     : %s \n" "$usedMemory"
-printf "Used Memory %%   : %s \n" "$usedMemoryPercent"
-
-
-
-#
-
-printf "\n" |awk "{printf $usedMemory / $totalMemory ;exit}"
-echo
-printf "\n" |awk "{printf ($usedMemory / $totalMemory)*100 ;exit}"
-echo
-
-
-
-
+printf "Used Memory %%   : %s%% \n" "$usedMemoryPercent"
 
 
