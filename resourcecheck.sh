db2584fef0fbf7993e201ac7b59d2aa47d00c1e5
chrotek <chrotek@gmail.com>
2019-05-31 10:17:01 +0200

    notes

---
+++ resourcecheck.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Check packages installed
+## sar
+
+# Find sar logs
+## Possible locations:
+### /var/log/sysstat
+### /var/log/sa
+
+# Check resources
+
+## Live 
+### Memory
+
+### CPU
+
+### DISK Space
